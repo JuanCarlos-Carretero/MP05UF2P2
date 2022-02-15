f13f8d61a64bfb06c2b07f0fc313729376b38969
Juan Carlos <jcarretero@elpuig.xeill.net>
2022-02-15 18:50:49 +0100

    v.2.1

--- src/test/java/ex2/HashTableTest.java
+++ src/test/java/ex2/HashTableTest.java
@@ -9,20 +9,20 @@ class HashTableTest {
     void count() {
         HashTable hashTable = new HashTable();
         Assertions.assertEquals(0, hashTable.count());
-        Assertions.assertEquals("", hashTable.toString());
     }
 
     @Test
     void size() {
         HashTable hashTable = new HashTable();
         Assertions.assertEquals(16, hashTable.size());
-        Assertions.assertEquals("", hashTable.toString());
     }
 
     @Test
     void put() {
         HashTable hashTable = new HashTable();
-        hashTable.put("1", "Perro");
+        hashTable.put("1", "Perro"); //colisiona y vuida
+        hashTable.put("17", "Dalmata"); //colisiona porque el maximo es 16 entonces en 17 vuelve a 1
+        hashTable.put("9", "Cocodrilo"); //no colisiona y no vuida
         Assertions.assertEquals("Perro", hashTable.get("1"));
         Assertions.assertEquals("\n bucket[1] = [1, Perro]", hashTable.toString());
     }
