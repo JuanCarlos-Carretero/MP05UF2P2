afe0762cbc67682422781fb658cebc87943e8508
Juan Carlos <jcarretero@elpuig.xeill.net>
2022-02-15 18:30:27 +0100

    v.2.1

--- src/test/java/ex2/HashTableTest.java
+++ src/test/java/ex2/HashTableTest.java
@@ -4,28 +4,32 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class HashTableTest {
-    HashTable hashTable = new HashTable();
+
     @Test
     void count() {
+        HashTable hashTable = new HashTable();
         Assertions.assertEquals(0, hashTable.count());
         Assertions.assertEquals("", hashTable.toString());
     }
 
     @Test
     void size() {
+        HashTable hashTable = new HashTable();
         Assertions.assertEquals(16, hashTable.size());
         Assertions.assertEquals("", hashTable.toString());
     }
 
     @Test
     void put() {
-        hashTable.put("1", "Cipote");
-        Assertions.assertEquals("Cipote", hashTable.get("1"));
-        Assertions.assertEquals("\n bucket[1] = [1, Cipote]", hashTable.toString());
+        HashTable hashTable = new HashTable();
+        hashTable.put("1", "Perro");
+        Assertions.assertEquals("Perro", hashTable.get("1"));
+        Assertions.assertEquals("\n bucket[1] = [1, Perro]", hashTable.toString());
     }
 
     @Test
     void get() {
+        HashTable hashTable = new HashTable();
         Assertions.assertNull(hashTable.get("1"));
         Assertions.assertNull(hashTable.get("0"));
         Assertions.assertEquals("", hashTable.toString());
@@ -33,7 +37,8 @@ class HashTableTest {
 
     @Test
     void drop() {
-        hashTable.put("1", "Cipote");
+        HashTable hashTable = new HashTable();
+        hashTable.put("1", "Perro");
         hashTable.drop("1");
         Assertions.assertNull(hashTable.get("1"));
     }
